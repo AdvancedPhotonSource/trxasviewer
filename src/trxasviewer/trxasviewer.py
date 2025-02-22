3efb54fe35fcd6265ddac587b78df6a3576e7d0a
Miaoqi Chu <mqichu@anl.gov>
2025-02-22 14:37:13 -0600

    add roi change and colormap update

--- src/trxasviewer/trxasviewer.py
+++ src/trxasviewer/trxasviewer.py
@@ -25,9 +25,11 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         self.setupUi(self)
         self.init_ui()
         self.image = None
+        self.roi = None
         self.current_dataset = TrXASDataset('/Users/mqichu/Documents/trxas/XTA_data/setup-full-00099')
         self.current_dataset.normalize()
         self.setup_imageview()
+        self.update_colormap()
         self.plot_dataset()
 
         self.model = QFileSystemModel()
@@ -37,6 +39,20 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         self.treeView_fs.setRootIndex(self.model.index(QDir.homePath()))
         self.treeView_fs.hideColumn(2)  # hide type
         # self.treeView_fs.hideColumn(3)  # hide Date
+        self.treeView_fs.selectionModel().selectionChanged.connect(self.selection_changed)
+        self.select_rawfolder('/Users/mqichu/Documents/trxas/XTA_data')
+        self.comboBox_cmap.currentIndexChanged.connect(self.update_colormap)
+        self.spinBox_roix.valueChanged.connect(self.update_roi)
+        self.spinBox_roiy.valueChanged.connect(self.update_roi)
+
+    def selection_changed(self, selected, deselected):
+        indexes = self.treeView_fs.selectionModel().selectedIndexes()
+        if indexes:
+            index = indexes[0]
+            file_path = self.model.filePath(index)
+            self.current_dataset = TrXASDataset(file_path)
+            self.current_dataset.normalize()
+            self.plot_dataset()
 
     def init_ui(self):
         self.pushButton_select_rawfolder.clicked.connect(self.select_rawfolder)
@@ -58,7 +74,25 @@ class MainWindow(QMainWindow, Ui_MainWindow):
                 # Initialize plots
         self.h_curve = self.pg_hdl_hline.plot(pen='r')
         self.v_curve = self.pg_hdl_vline.plot(pen='b')
-   
+        self.zoomin_image = pg.ImageItem()
+        self.pg_hdl_zoomin.addItem(self.zoomin_image)
+        self.pg_hdl_zoomin.setAspectLocked(False)
+    
+    def update_roi(self, value, position=None):
+        # value is a positional placeholder for the signal. It is not used.
+        roi_size = (self.spinBox_roix.value(), self.spinBox_roiy.value())
+        if self.roi is None:
+            self.roi = pg.RectROI([0, 0], roi_size, pen="r")
+            self.pg_hdl_img2d.addItem(self.roi)
+
+        # update size
+        if self.roi.size() != roi_size:
+            self.roi.setSize(roi_size)
+
+        if position is not None and self.roi.pos() != position:
+            self.roi.setPos((position[0] - roi_size[0] / 2,
+                             position[1] - roi_size[1] / 2))
+
     def mouseMoved(self, evt):
         pos = evt[0]
         if self.view.sceneBoundingRect().contains(pos):
@@ -86,15 +120,32 @@ class MainWindow(QMainWindow, Ui_MainWindow):
                 # Optional: Update plot ranges
                 self.pg_hdl_hline.setRange(xRange=[0, self.image.shape[1]])
                 # self.pg_hdl_vline.setRange(xRange=[0, self.image.shape[0]])
- 
+                self.update_roi(None, position=(x, y))
+            self.update_zoomed_view()
+
+    def update_zoomed_view(self):
+        if self.roi is None:
+            return
+        image_data = self.image 
+        roi_data = self.roi.getArrayRegion(image_data, self.pg_hdl_img2d.getImageItem())
+        if roi_data.size > 0:
+            self.zoomin_image.setImage(roi_data) 
+    
+    def update_colormap(self):
+        cmap = self.comboBox_cmap.currentText()
+        cmap = pg.colormap.getFromMatplotlib(cmap)
+        self.pg_hdl_img2d.setColorMap(cmap)
+        self.zoomin_image.setColorMap(cmap)
+    
     def plot_dataset(self):
         data = self.current_dataset.get_energy_vs_time(channel=1).T
         self.pg_hdl_img2d.getView().setAspectLocked(False)
         self.pg_hdl_img2d.setImage(data)
         self.image = data
     
-    def select_rawfolder(self):
-        folder_path = QFileDialog.getExistingDirectory(self, "Select Input Folder")
+    def select_rawfolder(self, folder_path=None):
+        if folder_path is None:
+            folder_path = QFileDialog.getExistingDirectory(self, "Select Input Folder")
         if folder_path:
             self.lineEdit_rawfolder.setText(folder_path)
             flist = os.listdir(folder_path)
