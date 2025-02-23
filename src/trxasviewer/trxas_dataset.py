b8d0b5e656dde9145a6b0e1f323d82065c9822d2
Miaoqi Chu <mqichu@anl.gov>
2025-02-23 01:33:20 -0600

    format the files

--- src/trxasviewer/trxas_dataset.py
+++ src/trxasviewer/trxas_dataset.py
@@ -1,16 +1,18 @@
 import re
-import numpy as np
-import matplotlib.pyplot as plt
 import time
 from functools import lru_cache
-TRXAS_PATTERN = re.compile(r'c(\d+)o(\d+)b(\d+)')
-P0 = 271555.0 # 271.555 kHz is P0 for APS, i.e. time of one orbit
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+TRXAS_PATTERN = re.compile(r"c(\d+)o(\d+)b(\d+)")
+P0 = 271555.0  # 271.555 kHz is P0 for APS, i.e. time of one orbit
 
 
 def is_sample_data(fname):
-    with open(fname, 'r') as f:
+    with open(fname, "r") as f:
         line = f.readline()
-        if re.match(r'#L .* Energy ', line):
+        if re.match(r"#L .* Energy ", line):
             return True
         else:
             return False
@@ -21,13 +23,13 @@ def process_header(header_line):
     # a typical header line
     # "#L N  Epoch  Energy  mono  monoE  undE ..."
     header = header_line[3:].strip()
-    if re.match(r'.* Energy ', header):
+    if re.match(r".* Energy ", header):
         dset_type = "Energy"
-    elif re.match(r'.* laserd ', header):
+    elif re.match(r".* laserd ", header):
         dset_type = "laserd"
     else:
         raise ValueError("Unknown dataset type")
-    
+
     labels = header.split()
     labels_mask = np.ones(len(labels), dtype=bool)
     record = []
@@ -45,10 +47,9 @@ def process_header(header_line):
 
     assert c_min == 0 and o_min == 0 and b_min == 0, "min index must be 0"
     shape = (c_max + 1, o_max + 1, b_max + 1)
-    index = record[:, 0] * shape[1] * shape[2] + \
-            record[:, 1] * shape[2] + record[:, 2]
+    index = record[:, 0] * shape[1] * shape[2] + record[:, 1] * shape[2] + record[:, 2]
     payload_mask = np.zeros(np.prod(shape), dtype=bool)
-    payload_mask[index] = True 
+    payload_mask[index] = True
     return dset_type, shape, labels, labels_mask, payload_mask
 
 
@@ -68,7 +69,7 @@ def fix_incomplete_dataset(payload_mask, shape, xas_data):
         The shape of the dataset when it's a complete dataset.
     xas_data : np.ndarray
         The dataset to be fixed, the incomplete part is filled with np.nan.
-    
+
     Returns
     -------
     xas_data : np.ndarray
@@ -94,31 +95,31 @@ class TrXASDatasetManager:
         self.delta_t_ns = None
         self.data_avg = None
         self.analysis_kwargs = None
-    
+
     def update_flist(self, flist):
         self.flist = flist
-    
+
     def save_results(self, fname):
         if self.analysis_kwargs is None:
             return
 
         results = {
-            'flist': self.flist,
-            'analysis_type': 'average_energy_vs_time',
-            'diff_data': self.data_avg,
-            'analysis_kwargs': self.analysis_kwargs,
-            'energy_axis': self.energy_axis,
-            'delta_t_ns': self.delta_t_ns,
+            "flist": self.flist,
+            "analysis_type": "average_energy_vs_time",
+            "diff_data": self.data_avg,
+            "analysis_kwargs": self.analysis_kwargs,
+            "energy_axis": self.energy_axis,
+            "delta_t_ns": self.delta_t_ns,
         }
         np.savez(fname, **results)
-    
-    def get_energy_vs_time(self, **kwargs): 
+
+    def get_energy_vs_time(self, **kwargs):
         if len(self.flist) == 0:
             return None, None, None
-        self.analysis_kwargs = kwargs 
+        self.analysis_kwargs = kwargs
         data = []
         shapes = []
-        for fname in self.flist: 
+        for fname in self.flist:
             if fname not in self.dsets_cache:
                 self.dsets_cache[fname] = TrXASDataset(fname, self.ignore_incomplete)
             t_data, energy, dt_ns = self.dsets_cache[fname].get_energy_vs_time(**kwargs)
@@ -130,31 +131,33 @@ class TrXASDatasetManager:
         data_full = np.full((len(self.flist), *shape_full), np.nan)
 
         for i, d in enumerate(data):
-            data_full[i, :d.shape[0], :d.shape[1]] = d
+            data_full[i, : d.shape[0], : d.shape[1]] = d
         data_avg = np.nanmean(data_full, axis=0)
 
         good_idx = np.argmin(np.prod(shapes, axis=1) == np.prod(shape_full))
 
         self.energy_axis = self.dsets_cache[self.flist[good_idx]].energy
-        self.delta_t_ns = self.dsets_cache[self.flist[good_idx]].delta_t_ns 
+        self.delta_t_ns = self.dsets_cache[self.flist[good_idx]].delta_t_ns
         self.data_avg = data_avg
-        return data_avg, self.energy_axis, self.delta_t_ns 
+        return data_avg, self.energy_axis, self.delta_t_ns
 
 
 class TrXASDataset:
     def __init__(self, fname, ignore_incomplete=True):
         self.fname = fname
-        with open(fname, 'r') as f:
+        with open(fname, "r") as f:
             for line in f:
-                if line.startswith('#L '): # Header line
-                    dset_type, shape, labels, labels_mask, payload_mask = process_header(line)
+                if line.startswith("#L "):  # Header line
+                    dset_type, shape, labels, labels_mask, payload_mask = (
+                        process_header(line)
+                    )
                     break
-        data = np.loadtxt(fname, comments='#', dtype=np.float32, delimiter='\t')
+        data = np.loadtxt(fname, comments="#", dtype=np.float32, delimiter="\t")
         self.num_energys = data.shape[0]
         self.labels = labels
         self.meta_data = data[:, labels_mask]
         self.dset_type = dset_type
-        self.energy = self.get('Energy')
+        self.energy = self.get("Energy")
 
         xas_part = data[:, ~labels_mask]
         # fill the missing data with NaN, create a whole dataset
@@ -169,67 +172,85 @@ class TrXASDataset:
         self.xas_data = xas_full.reshape(self.num_energys, self.shape[0], -1)
         self.xas_data_norm = self.normalize()
         self.xas_data_subgs = None
-    
-    def get_energy_vs_time(self, channel=0, target='raw', norm_kwargs=None):
-        if target == 'raw':
+
+    def get_energy_vs_time(self, channel=0, target="raw", norm_kwargs=None):
+        if target == "raw":
             return self.xas_data[:, channel], self.energy, self.delta_t_ns
-        elif target == 'normalized':
+        elif target == "normalized":
             return self.xas_data_norm, self.energy, self.delta_t_ns
-        elif target == 'normalized-GS':
-            if self.xas_data_subgs is None or norm_kwargs != self.xas_data_subgs.get('norm_kwargs'):
+        elif target == "normalized-GS":
+            if self.xas_data_subgs is None or norm_kwargs != self.xas_data_subgs.get(
+                "norm_kwargs"
+            ):
                 avg, diff, dt_ns = self.process_energy(**norm_kwargs)
                 self.xas_data_subgs = {
-                    'norm_kwargs': norm_kwargs,
-                    'avg_data': avg,
-                    'diff_data': diff,
-                    'dt_ns': dt_ns
+                    "norm_kwargs": norm_kwargs,
+                    "avg_data": avg,
+                    "diff_data": diff,
+                    "dt_ns": dt_ns,
                 }
-            return self.xas_data_subgs['diff_data'], self.energy, self.xas_data_subgs['dt_ns'] 
+            return (
+                self.xas_data_subgs["diff_data"],
+                self.energy,
+                self.xas_data_subgs["dt_ns"],
+            )
         else:
             raise ValueError("Unknown target")
-    
+
     def get(self, label):
         index = self.labels.index(label)
         return self.meta_data[:, index]
-    
+
     def get_temporal_coordinates(self, index):
         bunch = index % self.shape[0]
         orbital = index // self.shape[0]
         return (orbital, bunch)
 
     def normalize(self, repeat_rate=0):
-        acquire_time = self.get('Seconds')
+        acquire_time = self.get("Seconds")
         xas_data = np.copy(self.xas_data)
 
         if repeat_rate > 0:
             offset = xas_data / (repeat_rate * acquire_time)
             xas_data = -np.log(1.0 - offset)
 
-        xas_data = xas_data.reshape(self.num_energys, *self.shape)          # (rows, channel, orbital, bunch)
-        ortial_mean_ch0 = np.nanmean(xas_data[:, 0], axis=1)                #  rows x bunch 
-        xas_data[:, 1:] /= ortial_mean_ch0[:, np.newaxis, np.newaxis, :]    # normalize other channels
+        xas_data = xas_data.reshape(
+            self.num_energys, *self.shape
+        )  # (rows, channel, orbital, bunch)
+        ortial_mean_ch0 = np.nanmean(xas_data[:, 0], axis=1)  #  rows x bunch
+        xas_data[:, 1:] /= ortial_mean_ch0[
+            :, np.newaxis, np.newaxis, :
+        ]  # normalize other channels
         norm_data = xas_data.reshape(self.num_energys, self.shape[0], -1)
-        return np.mean(norm_data[:, 1:3], axis=(1,))    # average over channels 1 and 2
+        return np.mean(norm_data[:, 1:3], axis=(1,))  # average over channels 1 and 2
 
     def plot(self, channel=0, orbital=0, bunch=0):
         num_channels = self.shape[0]
         fig, ax = plt.subplots(1, num_channels, figsize=(4 * num_channels, 3))
-        extent=(self.energys[0], self.energys[-1], 0, np.prod(self.shape[1:]))
+        extent = (self.energys[0], self.energys[-1], 0, np.prod(self.shape[1:]))
 
         for i in range(num_channels):
-            ax[i].imshow(self.xas_data[:, i].T, aspect='auto', extent=extent)
-            ax[i].set_title(f'Channel {i}')
-            ax[i].set_xlabel('Energy (keV)')
-            ax[i].set_ylabel('XAS')
+            ax[i].imshow(self.xas_data[:, i].T, aspect="auto", extent=extent)
+            ax[i].set_title(f"Channel {i}")
+            ax[i].set_xlabel("Energy (keV)")
+            ax[i].set_ylabel("XAS")
         plt.show()
-        
-    def process_energy(self, fileout=None, trig_index=1820, pre_avg_orbitals=5, 
-                       aft_avg_bunches=11, do_perbunch='per_bunch'):
+
+    def process_energy(
+        self,
+        fileout=None,
+        trig_index=1820,
+        pre_avg_orbitals=5,
+        aft_avg_bunches=11,
+        do_perbunch="per_bunch",
+    ):
         if self.dset_type != "Energy":
-            raise TypeError(f"Expect Energy scan, but the file is {self.dset_type} scan.")
+            raise TypeError(
+                f"Expect Energy scan, but the file is {self.dset_type} scan."
+            )
 
         # average over the channels 1 and channel 2
-        data = self.xas_data_norm # num_energys * (orbitals * bunches)
+        data = self.xas_data_norm  # num_energys * (orbitals * bunches)
         num_orbitals = self.shape[1]
         num_bunches = self.shape[2]
 
@@ -239,25 +260,28 @@ class TrXASDataset:
             end = pos + (size - pos) // unit_len * unit_len
             pos = pos - start
             return pos, slice(start, end)
-        
-        trig_index, slice_pre = get_multiples(num_bunches * num_orbitals,
-                                              trig_index, num_bunches) 
-        data = data[:, slice_pre]    # num_energys * -1
+
+        trig_index, slice_pre = get_multiples(
+            num_bunches * num_orbitals, trig_index, num_bunches
+        )
+        data = data[:, slice_pre]  # num_energys * -1
         data = data.reshape(self.num_energys, -1, num_bunches)
 
         preavg_orbit_idx = trig_index // num_bunches
-        preavg_slice = slice(max(0, preavg_orbit_idx - pre_avg_orbitals), preavg_orbit_idx)
+        preavg_slice = slice(
+            max(0, preavg_orbit_idx - pre_avg_orbitals), preavg_orbit_idx
+        )
         # average along orbitals
         preavg = np.mean(data[:, preavg_slice], axis=(1,))  # num_energys * bunches
 
-        if do_perbunch == 'per_bunch':
+        if do_perbunch == "per_bunch":
             diff = data - preavg[:, np.newaxis, :]
-        elif do_perbunch == 'avg_bunch': 
+        elif do_perbunch == "avg_bunch":
             diff = data - np.mean(preavg, axis=1)[:, np.newaxis, np.newaxis]
         else:
             raise ValueError("Unknown do_perbunch value %s method")
-        
-        # apply binning 
+
+        # apply binning
         result = []
         num_elements = slice_pre.stop - slice_pre.start
         bin_index, slice_aft = get_multiples(num_elements, trig_index, aft_avg_bunches)
@@ -268,12 +292,12 @@ class TrXASDataset:
             result.append(x)
         return result[0], result[1], self.delta_t_ns * aft_avg_bunches
 
-            
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     # read_trsaxs_dataset('/Users/mqichu/Documents/trxas/XTA_data/setup-full-00099')
     for n in range(1):
         t0 = time.perf_counter()
-        dset = TrXASDataset('/Users/mqichu/Documents/trxas/XTA_data/setup-full-00099')
+        dset = TrXASDataset("/Users/mqichu/Documents/trxas/XTA_data/setup-full-00099")
         dset.normalize()
         # dset.plot()
         dset.process_energy()
--- src/trxasviewer/trxasviewer.py
+++ src/trxasviewer/trxasviewer.py
@@ -1,22 +1,22 @@
-from PySide6.QtWidgets import (
-    QApplication, QMainWindow, QWidget, QPushButton, QLabel, QVBoxLayout, 
-    QHBoxLayout, QLineEdit, QFileDialog, QMessageBox, QCheckBox, QComboBox,
-)
-from PySide6.QtGui import QAction, QFont
-from PySide6.QtWidgets import QFileSystemModel
-from PySide6.QtCore import QDir, Qt
-import sys
-import os
-import numpy as np
-from generated_ui import Ui_MainWindow
 import glob
-from trxas_dataset import TrXASDataset, TrXASDatasetManager
-import pyqtgraph as pg
+import os
+import sys
 
-pg.setConfigOption('background', 'w')
-pg.setConfigOption('foreground', 'k')
+import numpy as np
+import pyqtgraph as pg
+from generated_ui import Ui_MainWindow
+from PySide6.QtCore import QDir, Qt
+from PySide6.QtGui import QAction, QFont
+from PySide6.QtWidgets import (QApplication, QCheckBox, QComboBox, QFileDialog,
+                               QFileSystemModel, QHBoxLayout, QLabel,
+                               QLineEdit, QMainWindow, QMessageBox,
+                               QPushButton, QVBoxLayout, QWidget)
+from trxas_dataset import TrXASDataset, TrXASDatasetManager
+
+pg.setConfigOption("background", "w")
+pg.setConfigOption("foreground", "k")
 pg.setConfigOptions(antialias=True)
-pg.setConfigOptions(imageAxisOrder='row-major')
+pg.setConfigOptions(imageAxisOrder="row-major")
 
 
 class MainWindow(QMainWindow, Ui_MainWindow):
@@ -39,8 +39,10 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         self.treeView_fs.setRootIndex(self.model.index(QDir.homePath()))
         self.treeView_fs.hideColumn(2)  # hide type
         # self.treeView_fs.hideColumn(3)  # hide Date
-        self.treeView_fs.selectionModel().selectionChanged.connect(self.selection_changed)
-        self.select_rawfolder('/Users/mqichu/Documents/trxas/XTA_data')
+        self.treeView_fs.selectionModel().selectionChanged.connect(
+            self.selection_changed
+        )
+        # self.select_rawfolder('/Users/mqichu/Documents/trxas/XTA_data')
         self.comboBox_cmap.currentIndexChanged.connect(self.update_colormap)
         self.spinBox_roix.valueChanged.connect(self.update_roi)
         self.spinBox_roiy.valueChanged.connect(self.update_roi)
@@ -58,7 +60,7 @@ class MainWindow(QMainWindow, Ui_MainWindow):
 
     def init_ui(self):
         self.pushButton_select_rawfolder.clicked.connect(self.select_rawfolder)
-    
+
     def setup_imageview(self):
         # self.img2d_axes = pg.PlotItem()
         # self.pg_hdl_img2d.addItem(self.img2d_axes)
@@ -70,20 +72,20 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         self.hLine = pg.InfiniteLine(angle=0, movable=False)
         self.view.addItem(self.vLine, ignoreBounds=True)
         self.view.addItem(self.hLine, ignoreBounds=True)
-                # Initialize plots
-        self.h_curve = self.pg_hdl_hline.plot(pen='r')
-        self.pg_hdl_hline.setLabel('bottom', 'Energy', units='keV')
-        self.v_curve = self.pg_hdl_vline.plot(pen='b')
-        self.pg_hdl_vline.setLabel('left', 'Time', units='μs')
+        # Initialize plots
+        self.h_curve = self.pg_hdl_hline.plot(pen="r")
+        self.pg_hdl_hline.setLabel("bottom", "Energy", units="keV")
+        self.v_curve = self.pg_hdl_vline.plot(pen="b")
+        self.pg_hdl_vline.setLabel("left", "Time", units="μs")
         self.zoomin_image = pg.ImageItem()
         self.pg_hdl_zoomin.addItem(self.zoomin_image)
         self.pg_hdl_zoomin.setAspectLocked(False)
         self.pg_hdl_zoomin.hideAxis("bottom")
         self.pg_hdl_zoomin.hideAxis("left")
-        self.proxy = pg.SignalProxy(self.view.scene().sigMouseMoved,
-                                    rateLimit=60,
-                                    slot=self.mouseMoved)
-    
+        self.proxy = pg.SignalProxy(
+            self.view.scene().sigMouseMoved, rateLimit=60, slot=self.mouseMoved
+        )
+
     def update_roi(self, value, position=None):
         # value is a positional placeholder for the signal. It is not used.
         roi_size = (self.spinBox_roix.value(), self.spinBox_roiy.value())
@@ -96,36 +98,42 @@ class MainWindow(QMainWindow, Ui_MainWindow):
             self.roi.setSize(roi_size)
 
         if position is not None and self.roi.pos() != position:
-            self.roi.setPos((position[0] - roi_size[0] / 2,
-                             position[1] - roi_size[1] / 2))
+            self.roi.setPos(
+                (position[0] - roi_size[0] / 2, position[1] - roi_size[1] / 2)
+            )
 
     def mouseMoved(self, evt):
         pos = evt[0]
-        if self.image is None: return
+        if self.image is None:
+            return
         if self.view.sceneBoundingRect().contains(pos):
             mouse_point = self.view.mapSceneToView(pos)
             x = int(mouse_point.x())
             y = int(mouse_point.y())
-            
+
             # Update crosshair position
             self.vLine.setPos(x)
             self.hLine.setPos(y)
-            
+
             # Update line cuts if within image bounds
-            if (0 <= x < self.image.shape[1] and 0 <= y < self.image.shape[0]):
+            if 0 <= x < self.image.shape[1] and 0 <= y < self.image.shape[0]:
                 # Get horizontal and vertical cuts
                 horizontal_data = self.image[y, :]
                 vertical_data = self.image[:, x]
                 # Create y-axis values for vertical cut
                 x_positions = self.dset_manager.energy_axis
-                y_positions = np.arange(len(vertical_data)) * self.dset_manager.delta_t_ns / 1000 # us
+                y_positions = (
+                    np.arange(len(vertical_data)) * self.dset_manager.delta_t_ns / 1000
+                )  # us
                 # Update horizontal cut
                 self.h_curve.setData(x_positions, horizontal_data)
                 self.v_curve.setData(vertical_data, y_positions[::-1])
                 self.update_roi(None, position=(x, y))
                 energy = self.dset_manager.energy_axis[x]
                 t_time = (len(vertical_data) - y) * self.dset_manager.delta_t_ns / 1000
-                self.pg_hdl_zoomin.setTitle(f"Energy: {energy:.4f} keV, Time: {t_time:.3f} μs")
+                self.pg_hdl_zoomin.setTitle(
+                    f"Energy: {energy:.4f} keV, Time: {t_time:.3f} μs"
+                )
             self.update_zoomed_view()
 
     def update_zoomed_view(self):
@@ -135,27 +143,27 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         roi_data = self.roi.getArrayRegion(image_data, self.pg_hdl_img2d.getImageItem())
         if roi_data.size > 0:
             self.zoomin_image.setImage(np.flipud(roi_data))
-    
+
     def update_colormap(self):
         cmap = self.comboBox_cmap.currentText()
         cmap = pg.colormap.getFromMatplotlib(cmap)
         self.pg_hdl_img2d.setColorMap(cmap)
         self.zoomin_image.setColorMap(cmap)
-    
+
     def plot_dataset(self):
         kwargs = {
-            'channel': int(self.comboBox_channel_num.currentText()),
-            'target': self.comboBox_target.currentText(),
+            "channel": int(self.comboBox_channel_num.currentText()),
+            "target": self.comboBox_target.currentText(),
         }
-        if kwargs['target'] in ['normalized-GS']:
+        if kwargs["target"] in ["normalized-GS"]:
             norm_kwargs = {
-                'trig_index': self.spinBox_syncbunch_number.value(),
-                'do_perbunch': self.comboBox_groundstate_method.currentText(),
-                'pre_avg_orbitals': self.spinBox_orbitals_number.value(),
-                'aft_avg_bunches': self.spinBox_compress_bunches.value(),
+                "trig_index": self.spinBox_syncbunch_number.value(),
+                "do_perbunch": self.comboBox_groundstate_method.currentText(),
+                "pre_avg_orbitals": self.spinBox_orbitals_number.value(),
+                "aft_avg_bunches": self.spinBox_compress_bunches.value(),
             }
-            kwargs['norm_kwargs'] = norm_kwargs
-        if kwargs['target'] in ['normalized-GS', 'normalized']:
+            kwargs["norm_kwargs"] = norm_kwargs
+        if kwargs["target"] in ["normalized-GS", "normalized"]:
             self.comboBox_channel_num.setEnabled(False)
         else:
             self.comboBox_channel_num.setEnabled(True)
@@ -172,16 +180,15 @@ class MainWindow(QMainWindow, Ui_MainWindow):
                 self.spinBox_roix.setValue(data.shape[1] // 10)
                 self.spinBox_roiy.setValue(data.shape[0] // 10)
 
-            if kwargs['target'] in ['normalized-GS']:
+            if kwargs["target"] in ["normalized-GS"]:
                 vmin, vmax = np.percentile(data.ravel(), [0.1, 99.9])
             else:
                 vmin, vmax = np.percentile(data.ravel(), [0, 100])
             self.image = np.flipud(data)
             self.pg_hdl_img2d.setImage(self.image, levels=(vmin, vmax))
-    
+
     def select_rawfolder(self, folder_path=None):
-        if folder_path is None:
-            folder_path = QFileDialog.getExistingDirectory(self, "Select Input Folder")
+        folder_path = QFileDialog.getExistingDirectory(self, "Select Input Folder")
         if folder_path:
             self.lineEdit_rawfolder.setText(folder_path)
             flist = os.listdir(folder_path)
@@ -190,20 +197,20 @@ class MainWindow(QMainWindow, Ui_MainWindow):
             self.spinBox_fileindex_max.setValue(max(indexes))
             self.model.setRootPath(folder_path)
             self.treeView_fs.setRootIndex(self.model.index(folder_path))
-    
+
     def select_savefname(self):
         """
         Opens a QFileDialog to allow the user to select a save location for an NPZ file.
         Returns the selected file path with a '.npz' extension.
         """
         file_filter = "NumPy Compressed File (*.npz)"
-    
+
         # Open the file dialog
         filename, _ = QFileDialog.getSaveFileName(
             None,  # Parent widget (None for standalone)
             "Save File",  # Dialog title
             "",  # Default directory
-            file_filter  # Filter to only show .npz files
+            file_filter,  # Filter to only show .npz files
         )
 
         if filename and not filename.endswith(".npz"):
