8ef59b625b70da6ff576da79937699dacb5a5907
Miaoqi Chu <mqichu@anl.gov>
2025-03-09 21:35:32 -0500

    return prefix

--- src/trxasviewer/utilities.py
+++ src/trxasviewer/utilities.py
@@ -6,10 +6,18 @@ from .trxas_dataset import is_sample_data
 def get_valid_file_index(folder):
     """Returns a list of filenames in 'folder' that end with exactly five digits."""
     pattern = re.compile(r".*\d{5}$")  
-    
+    file_index = []
+    prefix = []
     try:
-        return [int(entry.name[-5:]) for entry in os.scandir(folder) if 
-                pattern.match(entry.name) and \
-                is_sample_data(os.path.join(folder, entry.name))]
+        for entry in os.scandir(folder):
+            if is_sample_data(os.path.join(folder, entry.name)):
+                if pattern.match(entry.name):
+                    file_index.append(int(entry.name[-5:]))
+                    prefix.append(entry.name[:-5])
+        file_index.sort()
+        prefix = list(set(prefix))
+        if len(prefix) > 1:
+            raise ValueError("Multiple prefixes found in folder")
+        return os.path.join(folder, prefix[0]), file_index
     except FileNotFoundError:
-        return [0, 1]
+        return 'none', [0, 1]
