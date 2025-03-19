50376771284123fac1b85baf85e626a6a5f91428
mqichu <mqichu@anl.gov>
2025-03-18 22:20:44 -0500

    add function to get scan types

--- src/trxasviewer/utilities.py
+++ src/trxasviewer/utilities.py
@@ -1,21 +1,68 @@
 import re
 from pathlib import Path
-from .trxas_dataset import is_sample_data
+import time
+from packaging.version import Version
+from functools import lru_cache
 
 
-def get_valid_file_index(folder):
-    """Returns a list of filenames in 'folder' that end with exactly five digits."""
-    pattern = re.compile(r".*\d{5}$")  
-    prefix_db = {}
+# @lru_cache(maxsize=1024)
+def get_scan_type(fname):
+    """
+    Determines the scan type from a file's first 10 lines.
     
-    for entry in Path(folder).iterdir():
-        if entry.is_file() and is_sample_data(entry):
-            if pattern.match(entry.name):
-                index = int(entry.name[-5:])
-                prefix = entry.name[:-5]
-                if prefix not in prefix_db:
-                    prefix_db[prefix] = [index]
-                else:
-                    prefix_db[prefix].append(index)
+    - If a line starts with "#S XXX exafs_scan", returns "exafs".
+    - If a line starts with "#S XXX rscan laserd", returns "laserd".
+    - Otherwise, or on exception, returns "invalid".
+    """
+    if Path(fname).is_dir():
+        # directory is need in treeview to display it as a folder/parents
+        return "directory"
+    try:
+        # if the file is being written, return "writing"
+        if not is_writing_done(fname):
+            return "writing"
+        pattern_exafs = re.compile(r"^#S\s+\d+\s+exafs_scan")
+        pattern_laserd = re.compile(r"^#S\s+\d+\s+rscan\s+laserd")
+        with open(fname, "r", encoding="utf-8", errors="replace") as f:
+            for _, line in zip(range(10), f):  # Read first 10 lines only
+                if pattern_exafs.match(line):
+                    return "exafs"
+                if pattern_laserd.match(line):
+                    return "laserd"
+    except (OSError, IOError):
+        return "invalid" 
+    return "invalid"
+
+
+def is_writing_done(fname, threshold=60):
+    """
+    Check if a file has not been modified in the last 'threshold' seconds.
     
-    return prefix_db
+    Returns True if the last modification was more than 'threshold' seconds ago.
+    """
+    file = Path(fname)
+    
+    if not file.exists():
+        return False  # File doesn't exist yet
+
+    last_modified = file.stat().st_mtime  # Get last modified time
+    elapsed_time = time.time() - last_modified  # Time since last modification
+
+    return elapsed_time > threshold  # True if more than 'threshold' seconds
+
+
+def compare_versions(version1, version2):
+    """
+    Compares two version strings.
+    Returns:
+        -1 if version1 < version2
+         0 if version1 == version2
+         1 if version1 > version2
+    """
+    v1 = Version(str(version1))
+    v2 = Version(str(version2))
+
+    if v1 < v2:
+        return False
+    elif v1 >= v2:
+        return True
