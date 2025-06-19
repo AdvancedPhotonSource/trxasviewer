7a968128804ddc24a82566034c096fc04ddf85f3
Miaoqi Chu <mqichu@anl.gov>
2025-06-19 01:40:55 -0500

    simplify num_workers logic

--- src/trxasviewer/constants.py
+++ src/trxasviewer/constants.py
@@ -8,3 +8,13 @@ TIME_SCALES = {
     "s": 1.0,
     "ks": 1e3,
 }
+
+SCALE_TO_TIME = {
+    1e-15: "fs",
+    1e-12: "ps",
+    1e-9: "ns",
+    1e-6: "µs",
+    1e-3: "ms",
+    1.0: "s",
+    1e3: "ks",
+}
